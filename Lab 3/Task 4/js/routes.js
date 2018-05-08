f0d445baeed7c26b826f480fdc69a78348f71596
Alnordh <alnordh@kth.se>
2018-05-08 08:42:13 +0200

    labb 5

--- Lab 3/Task 4/js/routes.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('init', function(event) {
-  var page = event.target;
-
-  if (page.id === 'mainpage') {
-    page.querySelector('#About').onclick = function() {
-      document.querySelector('#myNavigator').pushPage('about.html', {data: {title: 'About'}});
-    };
-  }
-});
