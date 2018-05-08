e727c3efa06dba37b32de4286f5074b0f046ecf3
Louise Grondahl <lougro@kth.se>
2018-05-08 09:28:03 +0200

    hämtar

--- Lab 3/Task 1/js/feet.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('init', function(event) {
-  var page = event.target;
-
-  if (page.id === 'mainpage') {
-    page.querySelector('#feet').onclick = function() {
-      document.querySelector('#myNavigator').pushPage('feetpage.html', {data: {title: 'Feet'}});
-    };
-  }
-});
